c4d49b9ef1c184974e647b10e2f8d836fc8beb2a
Andres Dominguez <andres.dominguez.p@gmail.com>
2017-02-04 23:32:25 -0500

    migrate to ava

--- test/jsdoc_transformer_test.js
+++ test/jsdoc_transformer_test.js
@@ -1,71 +1,70 @@
-const assert = require('assert');
+const test = require('ava');
 const JsDocTransformer = require('../lib/jsdoc_transformer');
 
-describe('js doc transformer', () => {
-  const comments = [
-    '/**',
-    ' * Hello world',
-    ' * @param {string} foo',
-    ' * @ngInject',
-    ' */',
-  ].join('\n');
+const comments = [
+  '/**',
+  ' * Hello world',
+  ' * @param {string} foo',
+  ' * @ngInject',
+  ' */',
+].join('\n');
 
-  it('adds param', () => {
-    const transformed = new JsDocTransformer(comments)
-        .addParam('john', '!SomeJohn')
-        .toString();
+test('adds param', (t) => {
+  const transformed = new JsDocTransformer(comments)
+      .addParam('john', '!SomeJohn')
+      .toString();
 
-    assert.equal(transformed,
-`/**
+  t.is(transformed,
+      `/**
  * Hello world
  * @param {string} foo
  * @param {!SomeJohn} john
  * @ngInject
  */`);
-  });
-
-  it('adds first param', () => {
-    // Given jsdoc without params.
-    const comments = [
-      '/**',
-      ' * Some stuff',
-      ' * @ngInject',
-      ' */',
-    ].join('\n');
-
-    // When you add param.
-    const transformed = new JsDocTransformer(comments)
-        .addParam('john', '!SomeJohn')
-        .toString();
-
-    // Then ensure the param was added.
-    assert.equal(transformed,
-`/**
- * Some stuff
- * @param {!SomeJohn} john
- * @ngInject
- */`);
-  });
-
-  it('adds ngInject and param', () => {
-    // Given jsdoc without params and ngInject.
-    const comments = [
-      '/**',
-      ' * Some stuff',
-      ' */',
-    ].join('\n');
-
-    // When you add param.
-    const transformed = new JsDocTransformer(comments)
-        .addParam('john', '!SomeJohn')
-        .toString();
-
-    // Then ensure the param was added and and @ngInject.
-    assert.equal(transformed,
-`/**
- * Some stuff
- * @param {!SomeJohn} john
- * @ngInject
- */`);
-  });
 });
+
+test('adds first param', (t) => {
+  // Given jsdoc without params.
+  const comments = [
+    '/**',
+    ' * Some stuff',
+    ' * @ngInject',
+    ' */',
+  ].join('\n');
+
+  // When you add param.
+  const transformed = new JsDocTransformer(comments)
+      .addParam('john', '!SomeJohn')
+      .toString();
+
+  // Then ensure the param was added.
+  t.is(transformed,
+      `/**
+ * Some stuff
+ * @param {!SomeJohn} john
+ * @ngInject
+ */`);
+});
+
+test('adds ngInject and param', (t) => {
+  // Given jsdoc without params and ngInject.
+  const comments = [
+    '/**',
+    ' * Some stuff',
+    ' */',
+  ].join('\n');
+
+  // When you add param.
+  const transformed = new JsDocTransformer(comments)
+      .addParam('john', '!SomeJohn')
+      .toString();
+
+  // Then ensure the param was added and and @ngInject.
+  t.is(transformed,
+      `/**
+ * Some stuff
+ * @param {!SomeJohn} john
+ * @ngInject
+ */`);
+});
+
