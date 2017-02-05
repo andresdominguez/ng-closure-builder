9a02ee55591ffa15d955fe2af89ea0bea321d6cf
Andres Dominguez <andres.dominguez.p@gmail.com>
2017-02-05 13:50:36 -0500

    Test for rejected promise

--- test/ns_finder_test.js
+++ test/ns_finder_test.js
@@ -7,3 +7,10 @@ test('finds namespace', t => {
       path.join(__dirname, '..', 'testdata', 'module-with-service.js');
   return nsFinder(filePath).then(found => t.is(found, 'foo.bar.FooBarModule'));
 });
+
+test('rejects promise when no goog.module is present', async t => {
+  const filePath =
+      path.join(__dirname, '..', 'testdata', 'random-module.js');
+  const error = await t.throws(nsFinder(filePath));
+  t.regex(error, /Cannot find goog.module in file/);
+});
