06628af195ab7b5bb40f60f92a7deaca429cd515
Andres Dominguez <andres.dominguez.p@gmail.com>
2017-02-04 23:06:53 -0500

    find different services

--- test/find_module_test.js
+++ test/find_module_test.js
@@ -3,9 +3,24 @@ const path = require('path');
 const findModule = require('./find_module');
 
 describe('Find module', () => {
+  const dir = path.join(__dirname, '..', 'testdata');
+
   it('finds service with module on same line', () => {
-    const dir = path.join(__dirname, '..', 'testdata');
     return findModule(dir, 'serviceOne').then(fileName => {
+      assert(fileName.indexOf('module-with-service.js') !== -1,
+          'File not found');
+    });
+  });
+
+  it('should finds service in multiple lines', function() {
+    return findModule(dir, 'serviceTwo').then(fileName => {
+      assert(fileName.indexOf('module-with-service.js') !== -1,
+          'File not found');
+    });
+  });
+
+  it('should finds service with string in next line', function() {
+    return findModule(dir, 'serviceThree').then(fileName => {
       assert(fileName.indexOf('module-with-service.js') !== -1,
           'File not found');
     });
