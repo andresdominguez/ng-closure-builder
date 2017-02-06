178f979a031d2d8b1b91cd00e27f4a4c82925e68
Andres Dominguez <andres.dominguez.p@gmail.com>
2017-02-05 19:34:15 -0500

    Add tests for components and assert promise rejection

--- test/find_module_test.js
+++ test/find_module_test.js
@@ -4,22 +4,30 @@ const findModule = require('../lib/find_module');
 
 const dir = path.join(__dirname, '..', 'testdata');
 
-const runFind = function(t, symbolName) {
-  return findModule(dir, symbolName, 'service')
-      .then(({fileName, namespace}) => {
-        t.true(fileName.indexOf('module-with-service.js') !== -1);
-        t.is(namespace, 'foo.bar.FooBarModule');
-      });
-};
-
-test('finds service with module on same line', t => {
-  return runFind(t, 'serviceOne');
+test('finds service with module on same line', async t => {
+  const result = await findModule(dir, 'serviceOne');
+  t.regex(result.fileName, /module-with-service.js/);
+  t.is(result.namespace, 'foo.bar.FooBarModule');
 });
 
-test('should finds service in multiple lines', t => {
-  return runFind(t, 'serviceTwo');
+test('finds service in multiple lines', async t => {
+  const result = await findModule(dir, 'serviceOne');
+  t.regex(result.fileName, /module-with-service.js/);
+  t.is(result.namespace, 'foo.bar.FooBarModule');
 });
 
-test('should finds service with string in next line', t => {
-  return runFind(t, 'serviceThree');
+test('finds service with string in next line', async t => {
+  const result = await findModule(dir, 'serviceOne');
+  t.regex(result.fileName, /module-with-service.js/);
+  t.is(result.namespace, 'foo.bar.FooBarModule');
+});
+
+test('finds component', async t => {
+  const result = await findModule(dir, 'compOne');
+  t.regex(result.fileName, /module-with-service.js/);
+  t.is(result.namespace, 'foo.bar.FooBarModule');
+});
+
+test('rejects promise when not found', async t => {
+  t.throws(findModule(dir, 'nonExistent'));
 });
